9582db40de60e46945aecf321dc5100f89d2660c
Nathan Stephens <nathan.yukio.stephens@gmail.com>
2022-04-13 15:57:10 -0700

    added users array and get and post requests for api

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const bodyParser =require('body-parser');
 
 // here is where we set up our express server connection to the local port
 const app = express();
 const port = process.env.PORT || 3001
 
+// use body parser to parse the json data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = [
+    {name: 'Johnny', email: 'johnny@onthespot.com'},
+    {name: 'Kevin', email: 'kevin@whereskevin.com'},
+    {name: 'Jason', email: 'jasony@campcrystallake.com'},
+    {name: 'Freddy', email: 'nightmare@elm.com'}
+];
+
 // this is the simple get request
 app.get('/', (req, res)  => {
     res.send("Nate's Express App")
 });
+
+// get all users
+app.get('/users', (req, res)  => {
+    res.json({ ok: true, users });
+});
+
+
+app.post('/adduser', (req, res)  => {
+    const { name, email } = req.body;
+    if (name && email) {
+        users.push({ name, email }) = req.body;
+        res.json({ok: true, users});
+    }
+}); 
+
 
 
 
